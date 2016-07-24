3f273376d17be742510a80712f88475ce3f662f8
George Ayris <george.ayris@huddle.com>
2016-07-24 21:25:17 +0100

    added production webpack

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-git checkout gh-pages && git merge master && webpack && git commit -am "gh-pages build" && git push && git checkout master
+git checkout gh-pages && git merge master && npm run build && git commit -am "gh-pages build" && git push && git checkout master
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -8,26 +8,19 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname + '/dist'),
+    path: path.resolve(__dirname),
     filename: 'bundle.js',
   },
-
-  watch: true,
 
   module: {
     loaders: [{
       test: /\.elm$/,
       exclude: [/elm-stuff/, /node_modules/],
-      loader: 'elm-hot!elm-webpack'
+      loader: 'elm-webpack'
     },{
       test:    /\.html$/,
       exclude: /node_modules/,
       loader:  'file?name=[name].[ext]',
     }]
-  },
-
-  devServer: {
-    inline: true,
-    stats: { colors: true },
   }
 };
