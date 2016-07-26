d229d32cca0c7b67b60474c8614fbb1cd1df6e95
Laurence Cook <lwc24@cam.ac.uk>
2016-07-26 19:12:19 +0100

    Adding python directory with test files

---
+++ pyteach/teach.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pdb
+
+import slate
+
+import pytesseract
+from PIL import Image
+
+#textstr = pytesseract.image_to_string(Image.open('units01.jpg'))
+#print textstr
+
+with open('units1.pdf') as f:
+	doc = slate.PDF(f)
+
+
+
+pdb.set_trace()
+
